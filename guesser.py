48f9e9ff3e4b58f5b2fc5aea6be9f93bcdedfed2
liyasolomon <liyasolomon08@gmail.com>
2024-05-23 21:38:23 -0400

    updated game

--- guesser.py
+++ guesser.py
@@ -11,34 +11,28 @@ def is_valid_guess(guess, guesses):
     return guess in guesses
 
 #evaluates the color of each letter depending on its location in the word
-def evaluate_guess(guess, word):
-    str = ""
+def evaluate_guess(guess, place):
+    
+    if guess != place : 
+        return "Try Again"
 
-    for i in range(5):
-        if guess[i] == word[i]:
-            str += "\033[32m" + guess[i]
-        elif guess[i] in word:
-            str += "\033[33m" + guess[i]
-        else: 
-            str += "\033[0m" + guess[i]
-
-    return str + "\033[0m" 
+    print("Congrats! You got it")
 
 #loads the game 
 def guesser(guesses, answers):
-    print("Welcome to CampusGueser! You have 6 chances to guess a 5 letter word.")
+    print("Welcome to CampusGueser! You have 6 chances to guess the locstion on campus.")
     place = random.choice(answers)
 
     attempts = 1
 
-    while(attempts <= 6):
+    while(attempts <= 3):
         guess = input ("Enter guess #" + str(attempts) + ": ").lower()
 
         if not is_valid_guess:
-            print("Invalid word. Please enter a 5 letter english word.")
+            print("Invalid location.")
             continue
         elif place == guess:
-            print("Congratulations! You guessed the word.")
+            print("Congratulations! You guessed the location.")
             break
         else:
             attempts +=1 
@@ -46,7 +40,7 @@ def guesser(guesses, answers):
             print(feedback)
 
     if attempts > 6:
-        print("Game over. The word was: ", place)
+        print("Game over. The location was: ", place)
         
 guesses = load_dictionary("guesses.txt")
 answers = load_dictionary("answers.txt")
