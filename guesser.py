20cd633ab02919188c887d2043819c44ef6a7200
liyasolomon <liyasolomon08@gmail.com>
2024-05-23 22:43:01 -0400

    functions with images

--- guesser.py
+++ guesser.py
@@ -1,49 +1,62 @@
 import random
+import os
+from PIL import Image
 
-#loads all the words in the file
+# Returns a dictionary where the keys are image filenames and the values are the corresponding locations
 def load_dictionary(file):
     with open(file) as f:
-        words = [line.strip() for line in f]
-        return words
+        data = {}
+        for line in f: 
+            filename, location = line.strip().split(',')
+            data[filename.strip()] = location.strip()
+        return data
+    
+# Display image
+def display_image(image_filename):
+    img = Image.open(image_filename)
+    img.show()
 
-#checks if this is a valid guess
+# Checks if this is a valid guess
 def is_valid_guess(guess, guesses):
     return guess in guesses
 
 #evaluates the color of each letter depending on its location in the word
-def evaluate_guess(guess, place):
+def evaluate_guess(guess, location):
     
-    if guess != place : 
+    if guess != location : 
         return "Try Again"
 
     print("Congrats! You got it")
 
 #loads the game 
-def guesser(guesses, answers):
-    print("Welcome to CampusGueser! You have 6 chances to guess the locstion on campus.")
-    place = random.choice(answers)
+def guesser(images):
+    print("Welcome to CampusGueser! You have 3 chances to guess the location on campus.")
+    image_filename, location = random.choice(list(images.items()))
+    display_image(image_filename)
 
     attempts = 1
 
     while(attempts <= 3):
         guess = input ("Enter guess #" + str(attempts) + ": ").lower()
+        
 
-        if not is_valid_guess:
+        if not is_valid_guess(guess, "guesses.txt"):
             print("Invalid location.")
             continue
-        elif place == guess:
+        elif guess == location:
             print("Congratulations! You guessed the location.")
             break
         else:
             attempts +=1 
-            feedback = evaluate_guess(guess, place)
+            feedback = evaluate_guess(guess, location)
             print(feedback)
 
     if attempts > 6:
-        print("Game over. The location was: ", place)
-        
-guesses = load_dictionary("guesses.txt")
-answers = load_dictionary("answers.txt")
+        print("Game over. The location was: ", location)
 
-guesser(guesses, answers)
+# Load images       
+images = load_dictionary("images.txt")
+
+# Start the game
+guesser(images)
 
