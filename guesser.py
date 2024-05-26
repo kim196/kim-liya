ee3239ef54a35fa923da2620ec68c51db400d4e8
liyasolomon <liyasolomon08@gmail.com>
2024-05-26 18:43:25 -0400

    added convention for name guessing & link to UMD building inventory

--- guesser.py
+++ guesser.py
@@ -19,51 +19,7 @@ def load_locations(file):
             filename, location = line.strip().split(',')
             data[filename.strip()] = location.strip()
         return data
-    
-# Display image
-def display_image(image_filename):
-    img = Image.open(image_filename)
-    img.show()
 
-# Checks if this is a valid guess
-def is_valid_guess(guess, guesses):
-    return guess in guesses
-
-#evaluates the color of each letter depending on its location in the word
-def evaluate_guess(guess, location):
-    
-    if guess != location : 
-        return "Try Again"
-
-    print("Congrats! You got it")
-
-#loads the game 
-def guesser(images):
-    print("Welcome to CampusGueser! You have 3 chances to guess the location on campus.")
-    image_filename, location = random.choice(list(images.items()))
-    display_image(image_filename)
-
-    attempts = 1
-
-    while(attempts <= 3):
-        guess = input ("Enter guess #" + str(attempts) + ": ").lower()
-
-        guesses = load_dictionary("guesses.txt")
-        
-
-        if not is_valid_guess(guess, guesses):
-            print("Invalid location.")
-            continue
-        elif guess == location:
-            print("Congratulations! You guessed the location.")
-            break
-        else:
-            attempts +=1 
-            feedback = evaluate_guess(guess, location)
-            print(feedback)
-
-    if attempts > 3:
-        print("Game over. The location was: ", location)
 
 # Path to Flask folder of images   
 image_dir = os.path.join(app.static_folder, 'images')
@@ -83,7 +39,7 @@ def home():
     map_url = url_for('static',filename=f'map/map.jpeg')
     return render_template("index.html",  map_url = map_url)
 
-# Game pages
+# Game page
 @app.route("/start_game")
 def game():
     img_url = url_for('static',filename=f'images/{location_img}')
@@ -95,13 +51,13 @@ def feedback( ):
     global attempts 
     guess = request.form['guess']
     
-    if guess.lower() != location:
+    if guess.upper() != location:
         if attempts < 3: 
             attempts =  attempts + 1
             img_url = url_for('static',filename=f'images/{location_img}')
             return render_template("try_again.html", image_url=img_url, num_attempts = 4 - attempts)
         else:
-            return "GAME OVER :("
+            return "GAME OVER :(  The location was: " + location
     else: 
         return "Congratulations! You guessed the location correctly :)"
    
